0dfcf92fcfbf40d63c0b77aed10366d595b29dfc
MwangiNgatiaJoseph <shinejoe727@gmail.com>
2026-04-02 13:28:53 +0300

    my wellness app

---
+++ app/src/main/java/com/zariel/chui_wellnessapp20/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.zariel.chui_wellnessapp20
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+//        Healthy recipe intent
+//        finding the view from the layout using their ids
+
+//        create a variable to store the buttons
+
+        val recipe=findViewById<Button>(R.id.recipes)
+
+//        set on  click listener
+
+        recipe.setOnClickListener {
+//            writing our intents
+
+             val recipeIntent= Intent(applicationContext, HealthyRecipe::class.java)
+            startActivity(recipeIntent)
+
+        }
+
+//        nutrition intent
+
+        val nutrition=findViewById<Button>(R.id.nutrition)
+
+        nutrition.setOnClickListener {
+
+            val nutritionIntent= Intent(applicationContext,NutritionAdvice::class.java)
+            startActivity(nutritionIntent)
+        }
+
+//        meditation intent
+
+        val meditation=findViewById<Button>(R.id.meditation)
+
+        meditation.setOnClickListener {
+
+            val meditationIntent= Intent(applicationContext, Meditation::class.java)
+            startActivity(meditationIntent)
+        }
+    }
+}
